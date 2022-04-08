bbd1f5f2ce7c7eb307f920d57af5b348409cd010
Beñat Castro <benatcastro@gmail.com>
2022-04-08 15:38:53 +0200

    split first word bug

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 13:30:59 by bena              #+#    #+#             */
-/*   Updated: 2022/04/08 14:11:17 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/08 15:35:11 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,22 @@
 #include "libft.h"
 
 size_t	ft_sub_size(char const *s, char c);
+size_t	ft_count_substrs(char const *s, char c);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**str;
 	size_t	count;
-	str = malloc(10);
-	count = 0;
+	if (!(str = malloc(ft_count_substrs(s, c) * sizeof(char *))))
+		return (NULL);
+	str[0] = malloc(ft_sub_size(s + 1, c));
+	ft_strlcpy(str[0], s, ft_sub_size(s, c) + 1);
+	count = 1;
 	while (*s)
 	{
-		if (*s == c && s[1] != c && count < ft_countchars(s, (char *)c))
+		if (*s == c && s[1] != c)
 		{
-			if (!(str[count] = malloc(ft_strlen(s))))
+			if (!(str[count] = malloc(ft_sub_size(s + 1, c))))
 				return (NULL);
 			str[count] = ft_substr(s + 1, 0, ft_sub_size(s + 1, c));
 			count ++;
@@ -44,8 +48,24 @@ size_t	ft_sub_size(char const *s, char c)
 	while (s[counter])
 	{
 		if (s[counter] == c)
-			return (counter);
+			break;
 		counter++;
 	}
+	//printf("Sub size: %ld\n", counter);
 	return (counter);
+}
+
+size_t	ft_count_substrs(char const *s, char c)
+{
+	size_t count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s == c && (s[1] != c && s[1] != '\0'))
+			count++;
+		s++;
+	}
+	//printf("sub count: %ld\n", count);
+	return (count);	
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 11:21:02 by becastro          #+#    #+#             */
-/*   Updated: 2022/04/08 14:03:45 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/08 15:37:18 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 #include <string.h>
 #include <ctype.h>
 
+size_t	ft_count_substrs(char const *s, char c);
+
+
 int	main(void)
 {
-	char	*str = "     lorem ipsum dolor sit amet";
+	char	*str = "   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ";
 	char 	c = ' ';
 	char	**splitted = ft_split(str, c);
-	for (size_t i = 0; i < ft_countchars(str, " "); i++)
+	for (size_t i = 0; i < ft_count_substrs(str, c) + 1; i++)
 	{
 		printf("%s\n", splitted[i]);
 	}
