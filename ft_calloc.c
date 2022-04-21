49b8316000c3193cd2341d8c7a7d2132fd0ef09e
Benat Castro <becastro@c3r5s1.42urduliz.com>
2022-04-22 01:53:33 +0200

    calloc test 4/4

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 10:41:21 by becastro          #+#    #+#             */
-/*   Updated: 2022/04/21 05:39:07 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/22 01:50:00 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 
-	if (count == 0)
-		return (NULL);
-	else if (count > 9223372036854775807 || size > 9223372036854775807)
-		return (NULL);
 	mem = malloc(size * count);
 	if (!mem)
 		return (NULL);
