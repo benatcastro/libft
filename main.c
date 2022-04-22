6612db193f2a698945adcc24f6fe994db1b1703c
Beñat Castro <benatcastro@gmail.com>
2022-04-23 00:06:36 +0200

    test

--- main.c
+++ main.c
@@ -1,30 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/28 11:21:02 by becastro          #+#    #+#             */
-/*   Updated: 2022/04/13 23:26:52 by bena             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
 size_t	ft_count_substrs(char const *s, char c);
 
-
-int	main(void)
+int main (void)
 {
-	char	*s1 = "asdasdas";
-	char s[] = "   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ";
-	char c = ' ';
-	//printf("Subs: %ld", ft_count_substrs(s, c));
-	char **split = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
-	for (size_t i = 0; i < ft_count_substrs(s, c); i++)	// {
-		printf("%s\n", split[i]);
+	char *s = "tripouille";
+	char c = 0;
+	char * * tab = ft_split(s, c);
+	printf("%s", tab[1]);
+	for (size_t i = 0; i < ft_count_substrs(s, c); i++)
+	{
+		printf("\ntab[%lu] => %s\n", i, tab[i]);
+	}
+	
 }
