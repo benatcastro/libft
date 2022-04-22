bc1d635df5f207b2bf642b32b6bac98ad3d6683a
Benat Castro <becastro@c3r5s1.42urduliz.com>
2022-04-22 04:44:25 +0200

    Split OKAY

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 13:30:59 by bena              #+#    #+#             */
-/*   Updated: 2022/04/21 07:31:00 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/22 04:42:01 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,46 @@
 #include <stdio.h>
 #include "libft.h"
 
-size_t	ft_sub_size(char const *s, char c);
-size_t	ft_count_substrs(char const *s, char c);
+size_t		ft_sub_size(char const *s, char c);
+size_t		ft_count_substrs(char const *s, char c);
+const char	*ft_next_word(const char *s, char c);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**str;
 	size_t	count;
-	size_t	i;
 
-	i = 0;
+	if (!s)
+		return (NULL);
 	str = malloc(ft_count_substrs(s, c) * sizeof(char *));
-	if (!(str))
+	if (!str)
 		return (NULL);
 	count = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			//printf("to print %s\n", s);
-			str[count] = ft_strldup(s, ft_sub_size(s, c));
-			count++;
-			s += ft_sub_size(s,c);
-			printf("TEST:%s\n", s);
+			str[count++] = ft_strldup(s, ft_sub_size(s, c));
+			s = ft_next_word(s, c);
+			if (*s == 0)
+				break ;
 		}
-		i++;
 		s++;
 	}
+	str[count] = 0;
 	return (str);
+}
+
+const char	*ft_next_word(const char *s, char c)
+{
+	while (*s != 0 && *s != c)
+		s++;
+	return (s);
 }
 
 size_t	ft_sub_size(char const *s, char c)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (*s && s[1] != c)
@@ -54,7 +61,6 @@ size_t	ft_sub_size(char const *s, char c)
 		i++;
 		s++;
 	}
-
 	return (i + 1);
 }
 
@@ -69,8 +75,5 @@ size_t	ft_count_substrs(char const *s, char c)
 			count++;
 		s++;
 	}
-	//printf("sub count: %ld\n", count);
-	if (count > 0)
-		return (count + 1);
-	return (count);
+	return (count + 1);
 }
