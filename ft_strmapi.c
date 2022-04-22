e67bbcb2f5a86e3a3a16fd03f19310f4b6921ef3
Benat Castro <becastro@c3r5s1.42urduliz.com>
2022-04-22 10:03:20 +0200

    strmapi tests ok

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,11 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 15:59:19 by bena              #+#    #+#             */
-/*   Updated: 2022/04/08 16:00:38 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/22 10:02:34 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char)
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	if (s == NULL || f == NULL)
+		return (NULL);
+	str = ft_strdup(s);
+	if (!(str))
+		return (NULL);
+	while (str[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	return (str);
+}
