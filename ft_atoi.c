f648c90709e1fdde87f584312fccf28b6b155c0a
Benat Castro <becastro@c3r5s1.42urduliz.com>
2022-04-22 09:21:02 +0200

    atoi norme

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 00:48:44 by bena              #+#    #+#             */
-/*   Updated: 2022/04/05 01:31:21 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/22 09:20:13 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	ft_atoi(const char *nptr)
 	nbr = 0;
 	i = 0;
 	sign = 1;
-	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\f' ||
-			nptr[i] == '\r' || nptr[i] == '\n' || nptr[i] == '\v')
+	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\f'
+		|| nptr[i] == '\r' || nptr[i] == '\n' || nptr[i] == '\v')
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
