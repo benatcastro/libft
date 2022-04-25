bd37f69d7657153b2b1ad47cac7ac3d2a1c8fa7f
Benat Castro <becastro@c3r2s6.42urduliz.com>
2022-04-25 09:35:28 +0200

    lstaddback

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 00:58:08 by bena              #+#    #+#             */
-/*   Updated: 2022/04/25 07:21:56 by bena             ###   ########.fr       */
+/*   Updated: 2022/04/25 09:32:06 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*aux;
 
-	if (lst)
+	if (new)
 	{
 		if (!*lst)
 		{
-			aux = ft_lstlast(*lst);
-			aux -> next = new;
-		}
-		else
 			*lst = new;
+			return ;
+		}
+		aux = ft_lstlast(*lst);
+		aux -> next = new;
 	}
 }
